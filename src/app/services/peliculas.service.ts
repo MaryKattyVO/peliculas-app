2b1481938693fc06ec342603caa0c55ee5f9b9e8
MaryKattyVO <mkvicunaore@gmail.com>
2021-07-17 18:00:46 -0500

    infiniteScroll de películas

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@ANGULAR/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
 
 @Injectable({
@@ -8,9 +9,25 @@ import { CarteleraResponse } from '../interfaces/cartelera-response';
 })
 export class PeliculasService {
 
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+  private carteleraPage = 1;
   constructor( private http: HttpClient) { }
 
+  get params() {
+    return {
+      api_key: '32e4b12b2832b65a2d406165f208b0ef',
+      language: 'es-ES',
+      page: this.carteleraPage.toString()
+    }
+  }
+
   getCartelera(): Observable<CarteleraResponse> {
-    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=32e4b12b2832b65a2d406165f208b0ef&language=es-ES&page=1')
+    return this.http.get<CarteleraResponse>(`${ this.baseUrl}/movie/now_playing?`, {
+      params: this.params
+    }).pipe(
+      tap( () => {
+        this.carteleraPage += 1;
+      })
+    )
   }
 }
