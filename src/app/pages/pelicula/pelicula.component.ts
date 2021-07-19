5d784a05a8896bc9a792fe2471f0cc47d3a97c5c
MaryKattyVO <mkvicunaore@gmail.com>
2021-07-18 19:35:43 -0500

    RXJS CombineLatest

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -4,6 +4,7 @@ import { PeliculasService } from '../../services/peliculas.service';
 import { MovieResponse } from '../../interfaces/movie-response';
 import { Location } from '@angular/common';
 import { Cast } from '../../interfaces/credits.response';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'app-pelicula',
@@ -25,18 +26,32 @@ export class PeliculaComponent implements OnInit {
     // const { id  }= this.activatedRoute.snapshot.params;
     // Sin desestructuración
     const id = this.activatedRoute.snapshot.params.id;
-    this.peliculasService.getPeliculaDetalle(id).subscribe(movie => {
-      if( !movie) {
-        this.router.navigateByUrl('/home')
-        return;
+
+    combineLatest([
+      this.peliculasService.getPeliculaDetalle(id),
+      this.peliculasService.getCast(id)
+    ]).subscribe( ([pelicula, cast]) => {
+      if( !pelicula) {
+            this.router.navigateByUrl('/home')
+            return;
       }
-      this.pelicula = movie;
+      this.pelicula = pelicula;
+      this.cast = cast.filter( actor => actor.profile_path != null );
     });
 
-    this.peliculasService.getCast(id).subscribe( cast => {
-      console.log(cast)
-      this.cast = cast.filter( actor => actor.profile_path != null );
-    })
+
+    // this.peliculasService.getPeliculaDetalle(id).subscribe(movie => {
+    //   if( !movie) {
+    //     this.router.navigateByUrl('/home')
+    //     return;
+    //   }
+    //   this.pelicula = movie;
+    // });
+
+    // this.peliculasService.getCast(id).subscribe( cast => {
+    //   console.log(cast)
+    //   this.cast = cast.filter( actor => actor.profile_path != null );
+    // })
   }
   onRegresar() {
       this.location.back();
